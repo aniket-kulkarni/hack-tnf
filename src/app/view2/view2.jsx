40a0263ca8f9281b27c0adf61907b3cea7c98415
Aniket Kulkarni <aniket@Triconinfotech.com>
2016-09-15 09:19:35 +0100

    Initial Setup

--- src/app/view2/view2.jsx
+++ src/app/view2/view2.jsx
@@ -1,5 +1,18 @@
 var React = require('react');
+import {observer} from "mobx-react";
+import {observable} from "mobx";
 
+class Test {
+    @observable name;
+    constructor() {
+        this.name = 'aniket'
+    }
+}
+
+var test = new Test();
+window.aniket = test;
+
+@observer
 class View2 extends React.Component {
 
     constructor(props) {
@@ -12,7 +25,7 @@ class View2 extends React.Component {
     render() {
         return (
             <div className="root">
-                I am View2
+                {test.name}
             </div>
         );
     }
