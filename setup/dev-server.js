153066bce408b7b57f944c2bdf03e4f46e0fd413
Aniket Kulkarni <aniket@Triconinfotech.com>
2016-09-29 21:54:12 +0100

    Added procfile

--- setup/dev-server.js
+++ setup/dev-server.js
@@ -4,7 +4,7 @@ var config = require('../webpack.config.dev');
 var open = require("open");
 var colors = require('colors');
 
-const port = 80;
+const port = process.env.port || 80;
 
 /* eslint-disable no-console */
 
